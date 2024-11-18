378ac0d0274b5adf9c859471dd84f01a0df959aa
Shreya <135098811+vaishre@users.noreply.github.com>
2024-11-18 13:36:49 +0530

    created package.java
    upload one file

---
+++ package.java
@@ -0,0 +1,15 @@
+class RupeesToPaise
+{
+  int getPaise(int rupees)
+  {
+     return rupees*100;
+  }
+}
+class PaiseToRupee
+{
+   int sendRupee(int paisa)
+   {
+      return Paisa/100;
+   }
+}
+             
